2ea0a91b695e9865587077ba77dcb1a8cb05de30
RashidKhan <rkecpsss50@gmail.com>
2026-02-14 18:47:36 +0530

    Allow all CORS origins temporarily for debugging

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: '*', // Allow all origins temporarily for debugging
     credentials: true
 }));
 app.use(express.json());
